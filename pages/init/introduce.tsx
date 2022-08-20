8a365604e7e6edb91fbc05d1e63e9a01a633fc7d
jason-choi-skku <jasonchoi3@g.skku.edu>
2022-08-21 01:29:47 +0900

    init done

---
+++ pages/init/introduce.tsx
@@ -0,0 +1,55 @@
+import {
+    Box,
+    Button,
+    Center,
+    Flex,
+    Input,
+    Text,
+    Textarea,
+    VStack,
+} from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Image from "next/image";
+import Router from "next/router";
+import { useState } from "react";
+import Logo from "../../assets/logo.png";
+
+const SignIn: NextPage = () => {
+    return (
+        <Center height={"95vh"} width="full">
+            <VStack width={"full"} margin={10} gap={12}>
+                <Image src={Logo} alt="logo" width={100} height={100} />
+                <VStack gap={0}>
+                    <Text>자기소개를 입력해 주세요.</Text>
+                </VStack>
+
+                <Box width={"full"}>
+                    <Textarea
+                        backgroundColor={"#f7f7f7"}
+                        borderWidth={0}
+                        minHeight={200}
+                        resize={"none"}
+                        
+                    />
+                </Box>
+
+                <Flex gap={4}>
+                    <Button
+                        width={144}
+                        height={42}
+                        borderRadius={24}
+                        marginTop={4}
+                        colorScheme="brand"
+                        onClick={() => {
+                            Router.push("/init/matching");
+                        }}
+                    >
+                        입력 하기
+                    </Button>
+                </Flex>
+            </VStack>
+        </Center>
+    );
+};
+
+export default SignIn;
---
+++ pages/init/matching.tsx
@@ -0,0 +1,56 @@
+import {
+    Box,
+    Button,
+    Center,
+    Flex,
+    Input,
+    Text,
+    Textarea,
+    VStack,
+} from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Image from "next/image";
+import Router from "next/router";
+import { useEffect, useState } from "react";
+import Logo from "../../assets/logo.png";
+import { motion } from "framer-motion";
+
+const SignIn: NextPage = () => {
+    useEffect(() => {
+        setTimeout(() => {
+            Router.push("/");
+        }, 3000);
+    });
+
+    return (
+        <Center height={"95vh"} width="full">
+            <VStack width={"full"} margin={10} gap={12}>
+                <motion.div
+                    className="block"
+                    animate={{
+                        rotate: 360,
+                    }}
+                    transition={{
+                        duration: 1,
+                        repeat: Infinity,
+                        ease: "linear",
+                    }}
+                >
+                    <Image src={Logo} alt="logo" width={200} height={200} />
+                    </motion.div>
+                
+                <VStack
+                    gap={0}
+                    color="brand.100"
+                    fontWeight={600}
+                    fontSize={24}
+                >
+                    <Text>통하는 팀원을 찾고 있어요!</Text>
+                    <Text>잠시만 기다려 주세요 :)</Text>
+                </VStack>
+            </VStack>
+        </Center>
+    );
+};
+
+export default SignIn;
